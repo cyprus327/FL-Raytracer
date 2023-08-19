91cf7296ab139044c49290515397a470cabc0a60
cyprus <jrcjr327@gmail.com>
2023-08-19 01:24:37 -0400

    Added AABBs

--- Program.cs
+++ Program.cs
@@ -19,11 +19,6 @@ internal sealed class Raytracer {
             MatInd = 1
         },
         new Sphere() {
-            Pos = new(0f, -0.35f, 0.35f),
-            Radius = 0.35f,
-            MatInd = 2
-        },
-        new Sphere() {
             Pos = new(0f, 1000f, 0f),
             Radius = 1000f,
             MatInd = 3
@@ -55,6 +50,19 @@ internal sealed class Raytracer {
         }
     };
 
+    private static readonly AABB[] _aabbs = {
+        new AABB() {
+            Min = new(1.2f, 0f, -0.6f),
+            Max = new(0.6f, -2f, -1.2f),
+            MatInd = 0
+        },
+        new AABB() {
+            Min = new(-1.2f, 0f, -0.6f),
+            Max = new(-0.6f, -2f, -1.2f),
+            MatInd = 1
+        }
+    };
+
     private static readonly Material[] _materials = {
         new Material() {
             Albedo = new(0.62f, 0.87f, 0.64f),
@@ -73,18 +81,18 @@ internal sealed class Raytracer {
         },
         new Material() {
             Albedo = new(0.9f, 0.9f, 0.9f),
-            Metallic = 0.4f,
-            Roughness = 0.4f,
+            Metallic = 0.05f,
+            Roughness = 0.95f,
         },
         new Material() {
             Albedo = new(1f, 0.7f, 0.2f),
-            Metallic = 0.6f,
-            Roughness = 0.4f,
+            Metallic = 0.2f,
+            Roughness = 0.8f,
         },
         new Material() {
             Albedo = new(0.2f, 0.4f, 0.9f),
-            Metallic = 0.6f,
-            Roughness = 0.4f,
+            Metallic = 0.2f,
+            Roughness = 0.8f,
         },
         new Material() {
             Albedo = Vector3.One,
@@ -93,7 +101,18 @@ internal sealed class Raytracer {
     };
 
     public static void Main() {
-        FL.Init(800, 450, "raytracer", PerPixel, PerFrame);
+        Vector3 centerPoint = Vector3.Zero;
+        Vector3 referencePoint = new(0f, -1.25f, 0f);
+        Vector3 rotationAxis = Vector3.Normalize(referencePoint - centerPoint);
+
+        Matrix4x4 rotMat = Matrix4x4.CreateFromAxisAngle(rotationAxis, MathF.PI / 9f);
+
+        _spheres[3].Pos = Vector3.Transform(_spheres[3].Pos - centerPoint, rotMat) + centerPoint;
+        _spheres[4].Pos = Vector3.Transform(_spheres[4].Pos - centerPoint, rotMat) + centerPoint;
+        _spheres[5].Pos = Vector3.Transform(_spheres[5].Pos - centerPoint, rotMat) + centerPoint;
+        _spheres[6].Pos = Vector3.Transform(_spheres[6].Pos - centerPoint, rotMat) + centerPoint;
+
+        FL.Init(800, 450, "Raytracer", PerPixel, PerFrame);
         FL.Run();
     }
 
@@ -140,6 +159,15 @@ internal sealed class Raytracer {
             return FL.Black;
         }
 
+        // Vector3 normalColor = Vector3.One * 0.5f;
+
+        // HitPayload payload = TraceRay(rayOrigin, rayDir);
+        // if (payload.HitDist != -1f) {
+        //     normalColor = (Vector3.Normalize(payload.WorldNormal) + Vector3.One) * 0.5f;
+        // }
+
+        // return FL.NewColor(normalColor);
+
         Vector3 light = Vector3.Zero;
         Vector3 contribution = Vector3.One;
 
@@ -151,8 +179,8 @@ internal sealed class Raytracer {
                 break;
             }
 
-            Sphere sphere = _spheres[payload.ObjectInd];
-            Material mat = _materials[sphere.MatInd];
+            int ind = payload.ObjectInd;
+            Material mat = _materials[ind < _spheres.Length ? _spheres[ind].MatInd : _aabbs[ind - _spheres.Length].MatInd];
             
             light += mat.Emission * contribution;
             contribution *= mat.Albedo;
@@ -168,8 +196,9 @@ internal sealed class Raytracer {
     }
 
     private static HitPayload TraceRay(Vector3 rayOrigin, Vector3 rayDir) {
-        int closestSphereInd = -1;
+        int closestObjInd = -1;
         float hitDist = float.MaxValue;
+        
         for (int i = 0; i < _spheres.Length; i++) {
             Sphere sphere = _spheres[i];
 
@@ -189,15 +218,39 @@ internal sealed class Raytracer {
             float closestT = (-b - MathF.Sqrt(disc)) / (2f * a);
             if (closestT < hitDist && closestT > 0) {
                 hitDist = closestT;
-                closestSphereInd = i;
+                closestObjInd = i;
             }
         }
 
-        if (closestSphereInd == -1) {
+        for (int i = 0; i < _aabbs.Length; i++) {
+            AABB aabb = _aabbs[i];
+
+            Vector3 invRayDir = new(1f / rayDir.X, 1f / rayDir.Y, 1f / rayDir.Z);
+            Vector3 tMin = (aabb.Min - rayOrigin) * invRayDir;
+            Vector3 tMax = (aabb.Max - rayOrigin) * invRayDir;
+
+            Vector3 t1 = Vector3.Min(tMin, tMax);
+            Vector3 t2 = Vector3.Max(tMin, tMax);
+
+            float tNear = MathF.Max(MathF.Max(t1.X, t1.Y), t1.Z);
+            float tFar = MathF.Min(MathF.Min(t2.X, t2.Y), t2.Z);
+
+            if (tNear > tFar || tFar < 0) {
+                continue;
+            }
+
+            float t = tNear > 0 ? tNear : tFar;
+            if (t < hitDist) {
+                hitDist = t;
+                closestObjInd = i + _spheres.Length;
+            }
+        }
+
+        if (closestObjInd == -1) {
             return HitPayload.MissPayload;
         }
 
-        return ClosestHit(rayOrigin, rayDir, hitDist, closestSphereInd);
+        return ClosestHit(rayOrigin, rayDir, hitDist, closestObjInd);
     }
 
     private static HitPayload ClosestHit(Vector3 rayOrigin, Vector3 rayDir, float hitDist, int objectInd) {
@@ -206,12 +259,33 @@ internal sealed class Raytracer {
             ObjectInd = objectInd
         };
 
-        ref Sphere sphere = ref _spheres[objectInd];
+        Vector3 hitPoint;
+        Vector3 normal;
 
-        payload.WorldPos = rayOrigin - sphere.Pos + rayDir * hitDist;
-        payload.WorldNormal = Vector3.Normalize(payload.WorldPos);
+        if (objectInd < _spheres.Length) {
+            hitPoint = rayOrigin + rayDir * hitDist;
+            normal = Vector3.Normalize(hitPoint - _spheres[objectInd].Pos);
+        } else {
+            AABB aabb = _aabbs[objectInd - _spheres.Length];
 
-        payload.WorldPos += sphere.Pos;
+            hitPoint = rayOrigin + rayDir * hitDist;
+
+            Vector3 center = (aabb.Min + aabb.Max) * 0.5f;
+            Vector3 extents = aabb.Max - center;
+            Vector3 hitPointLocal = hitPoint - center;
+
+            float maxAbs = MathF.Max(MathF.Abs(hitPointLocal.X / extents.X), MathF.Max(MathF.Abs(hitPointLocal.Y / extents.Y), MathF.Abs(hitPointLocal.Z / extents.Z)));
+
+            if (MathF.Abs(hitPointLocal.X / extents.X) == maxAbs)
+                normal = new Vector3(MathF.Sign(hitPointLocal.X), 0, 0);
+            else if (MathF.Abs(hitPointLocal.Y / extents.Y) == maxAbs)
+                normal = new Vector3(0, MathF.Sign(hitPointLocal.Y), 0);
+            else
+                normal = new Vector3(0, 0, MathF.Sign(hitPointLocal.Z));
+        }
+
+        payload.WorldPos = hitPoint;
+        payload.WorldNormal = normal;
 
         return payload;
     }
@@ -219,6 +293,13 @@ internal sealed class Raytracer {
     private struct Sphere {
         public Vector3 Pos;
         public float Radius;
+        
+        public int MatInd;
+    }
+
+    private struct AABB {
+        public Vector3 Min;
+        public Vector3 Max;
         
         public int MatInd;
     }
